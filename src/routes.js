333cd8e54426f416175657d8d7499fbd7992ebf7
vuvufelix <vuvufelix7@gmail.com>
2025-11-21 14:41:42 -0200

    feat: add rota principal

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,9 @@ import ProductController from "./app/controllers/ProductController.js";
 
 const router = new Router();
 
+router.get("/", (req, res) => {
+    res.json({message: "API rodando conectado com o clever cloud!"})
+})
 router.post("/create-checkout-session", ProductController.checkoutSession)
 router.get("/products", ProductController.index);
 router.get("/product/:id", ProductController.show);
